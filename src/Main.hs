6b9b2bba9398d6cb2a8ce76720067d33922d0b55
Anurag Ohri <anuragohri@ubuntu.ubuntu-domain>
2017-01-07 20:18:16 +0530

    Changing the error message for unsolvable puzzles.

--- src/Main.hs
+++ src/Main.hs
@@ -14,11 +14,18 @@ main = do
   getLine >>= \input -> do
     let
       list = map (\x -> read (x : "") :: Int) (filter (' ' /= ) input)
+      board = fromList list (fromJust $ elemIndex 0 list)
       solution = astar
-        (fromList list (fromJust $ elemIndex 0 list))
+        board
         (fromList [1,2,3,4,5,6,7,8,0] 8)
         neighbors
         manhattan
         uid
-    mapM_ (\(i,s) -> putStrLn ("-- " ++ show i ++ " --") >> putStrLn s) $ zip [1..] $ map show solution
-    putStrLn ("Steps Taken : " ++ show (length solution - 1))
+    if isSolvable board
+      then
+        do
+          mapM_ (\(i,s) -> putStrLn ("-- " ++ show i ++ " --") >> putStrLn s) $ zip [1..] $ map show solution
+          putStrLn ("Steps Taken : " ++ show (length solution - 1))
+      else
+        do
+          putStrLn "This 8-puzzle cannot be solved. It cannot be converted to the goal state showed above. Try swapping two non-zero adjacent integers."          
